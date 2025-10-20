6714cf6dec2d50a340ac5b390d6ef50964a89862
YosraZiad <yosra774829@gmail.com>
2025-10-20 09:54:36 +0300

    setup environment

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'import/order': ['warn', { 'newlines-between': 'always' }],
+  },
+}
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  }
+}
---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,12 @@
+## Summary
+
+Describe the change and why it is needed.
+
+## How to test
+
+1. Steps to reproduce
+2. Expected results
+
+## Checklist
+- [ ] My code follows the style guidelines
+- [ ] I have performed a self-review of my code
