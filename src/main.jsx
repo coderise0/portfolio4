48d8cfacfcac6296dd7488edbb93aaebc284e33c
Mais <maisshanbour.4@gmail.com>
2025-10-23 18:27:30 +0300

    feat: update main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createRoot } from 'react-dom/client'
-import './styles/colors.css'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './styles/colors.css';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
