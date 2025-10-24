4ae095813949285686ecc6ed46bf17ec040ad42a
RubaALmahmoud <rubaalzoubi31@gmail.com>
2025-10-24 21:35:06 +0300

    save this changes

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './styles/colors.css';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './sections/Hero';
 import AboutMe from './sections/AboutMe';
 import { Testimonials } from './sections/Testimonials.jsx';
+import ContactForm from './sections/ContactForm';
 import FooterSection from './sections/Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <AboutMe/>
+    <Hero />
+    <AboutMe />
     <Testimonials />
+    <ContactForm />
     <FooterSection />
   </StrictMode>
 );
